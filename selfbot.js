fc3babeef11e6c06291e8bebe28c664bcc332352
spicyhot <33399873+spicyhot@users.noreply.github.com>
2018-01-17 02:46:06 +0300

    Update selfbot.js

--- selfbot.js
+++ selfbot.js
@@ -9,4 +9,4 @@ bot.on('message', message => {
         message.edit('http://image.prntscr.com/image/2d4fe95944fd48e4b92b25db63dd3f4c.png');
         }
 });
-bot.login("MTI1MjQyMjIwNzM4NTEwODQ4.DHvLbQ.81PjnhkUmxVXUOZaM70fCt2q-s8");
+bot.login(process.env.BOT_TOKEN);
