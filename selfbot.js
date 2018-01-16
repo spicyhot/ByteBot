192e1c61b087bceb17e05fe73e68f84693818f85
spicyhot <33399873+spicyhot@users.noreply.github.com>
2018-01-17 02:52:09 +0300

    Update selfbot.js

--- selfbot.js
+++ selfbot.js
@@ -8,5 +8,10 @@ bot.on('message', message => {
     if (message.content===',killjoao') {
         message.edit('http://image.prntscr.com/image/2d4fe95944fd48e4b92b25db63dd3f4c.png');
         }
+  
+bot.on('message', message => {
+    if (message.content===',killjoao') {
+        message.edit('https://i.imgur.com/NksGlzB.gifv');
+        }
 });
 bot.login(process.env.BOT_TOKEN);
